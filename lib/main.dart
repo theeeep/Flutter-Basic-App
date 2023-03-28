039d8de332854fc80f91b52d17fd638856fc65bd
Deepak <ideepakrabindra@gmail.com>
2023-03-28 17:50:45 +0530

    Textspan

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,31 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+    MaterialApp(
       title: "My FLutter App",
       home: Scaffold(
-        appBar: AppBar(title: Text('Basic App')),
+        appBar: AppBar(
+          title: Text('Basic App'),
+          backgroundColor: Colors.blueGrey,
+        ),
         body: Center(
-          child: Text(
-            'Heeello World',
-            style: TextStyle(fontSize: 20.0),
+          child: Text.rich(
+            // ignore: prefer_const_literals_to_create_immutables
+            TextSpan(text: "My", children: [
+              TextSpan(
+                  text: 'Flutter',
+                  style:
+                      TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold)),
+              TextSpan(
+                  text: 'App',
+                  style: TextStyle(fontSize: 30.0, color: Colors.blueAccent)),
+            ]),
           ),
         ),
-      )));
+      ),
+    ),
+  );
 }
