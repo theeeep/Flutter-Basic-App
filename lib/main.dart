c6c39f8c8ea20a9613f7377aaaf456207425f164
Deepak <ideepakrabindra@gmail.com>
2023-03-28 20:01:06 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: "My FLutter App",
+        themeMode: ThemeMode.system,
         debugShowCheckedModeBanner: false,
         home: DashBoard());
   }
