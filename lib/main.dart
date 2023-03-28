ab3c6213558b02274f2a7b38ec08a7281f63341b
Deepak <ideepakrabindra@gmail.com>
2023-03-28 17:31:32 +0530

    a

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "My FLutter App",
-    home: Center(
-      child: Text(
-        'Hello World',
-        textDirection: TextDirection.ltr,
-      ),
-    ),
-  ));
+      title: "My FLutter App",
+      home: Scaffold(
+        appBar: AppBar(title: Text('Basic App')),
+        body: Center(
+          child: Text(
+            'Heeello World',
+            style: TextStyle(fontSize: 20.0),
+          ),
+        ),
+      )));
 }
